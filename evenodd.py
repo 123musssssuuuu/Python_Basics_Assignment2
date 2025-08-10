1d405489390450768d06b99f26195ca8700587e2
muskan bansal <166107530+123musssssuuuu@users.noreply.github.com>
2025-08-10 15:52:29 +0530

    Add files via upload

---
+++ evenodd.py
@@ -0,0 +1,6 @@
+num =int(input("Enter the number:"))
+
+if num% 2 ==0:
+    print(num,"is an even nummber")
+else:
+    print(num,"is an odd number")
---
+++ sum_of_integers.py
@@ -0,0 +1,10 @@
+
+# Initial sum value
+total_sum = 0
+
+# Loop through numbers from 1 to 50
+for i in range(1, 51):
+    total_sum += i
+
+# Display final sum
+print(f"The sum of integers from 1 to 50 is: {total_sum}")
